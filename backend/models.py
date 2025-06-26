02368e242bca44e7016704a02516f5500d6031f5
Simanta Ray <simanta.ray@tigeranalytics.com>
2025-06-26 12:14:44 +0530

    product db

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String
-from db import Base
+from sqlalchemy import Column, DateTime, Float, Integer, String, func
+from backend.db import Base
 
 class Student(Base):
     __tablename__ = "students"
@@ -8,3 +8,13 @@ class Student(Base):
     name = Column(String)
     email = Column(String)
     password = Column(String)
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    description = Column(String, nullable=True)
+    date_added = Column(DateTime(timezone=True), server_default=func.now())
