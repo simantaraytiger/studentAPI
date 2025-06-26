24ab448bac8d39b544e6a86e660594ca9d836734
Simanta Ray <simanta.ray@tigeranalytics.com>
2025-06-26 12:13:21 +0530

    database name change

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite+aiosqlite:///./students.db"
+DATABASE_URL = "sqlite+aiosqlite:///./database.db"
 
 # Create async engine
 engine = create_async_engine(
